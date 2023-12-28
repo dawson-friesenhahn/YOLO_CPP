af9a73773be37825bea27f77f745a4cdb93dfc0e
Dawson Friesenhahn <dawson.friesenhahn.1@spaceforce.mil>
2023-12-28 14:13:05 -0500

    min reporoducible example for difference in YOLOv8 Inferences with CPU vs GPU

--- src/main.cpp
+++ src/main.cpp
@@ -8,28 +8,11 @@
 
 int main() {
    
-   
-   //Uncomment for v8ObjDetection
-   //std::unique_ptr<YOLO_Abstract> yolo = std::make_unique<YOLO_v8ObjDetection>(); //goal is to be able to easily sub in a new model architecture here
-   //yolo->loadModel( "../models/v8_objDetection.onnx" );
+   std::unique_ptr<YOLO_Abstract> yolo = std::make_unique<YOLO_v8ObjDetection>(); 
+   yolo->loadModel( "../models/v8_objDetection.onnx" );
   
-   //Uncomment for v5ObjDetection
-   //std::unique_ptr<YOLO_Abstract> yolo = std::make_unique<YOLO_v5ObjDetection>();
-   //yolo->loadModel( "../models/v5_objDetection.onnx" );
-
-   //Uncomment for v8Pose
-   std::unique_ptr<YOLO_Abstract> yolo = std::make_unique<YOLO_v8Pose>();
-   yolo->loadModel( "../models/v8_Pose.onnx" );
-   
    
    cv::Mat inputImg = cv::imread( "../images/f16_1.png" );
-
-
-   //Uncomment for v8Pose (multi-model). Same class is being used (YOLOv_v8Pose), but I left these 3 lines here to show how I used it.
-   //std::unique_ptr<YOLO_Abstract> yolo = std::make_unique<YOLO_v8Pose>();
-   //yolo->loadModel( "../models/v8_pose_multiClass.onnx" );
-   //dynamic_cast<YOLO_v8Pose*>(yolo.get())->setNumClasses( 3 ); //I don't like this line, but I couldn't think of a concise way to determine the number of classes just by parsing the onnx file. I'm sure with some thought, we could figure it out.
-   //cv::Mat inputImg = cv::imread( "../images/capture0.jpeg" );
    
    cv::Mat outputImg = cv::Mat();
 
@@ -38,12 +21,20 @@ int main() {
    for( const auto& detection : detections ) {
       std::cout << detection << "\n";
    }
-
-
-   cv::imshow( "Input:", inputImg );
+   
+   cv::imshow( "Output (CUDA Backend):", outputImg );
    cv::waitKey( 0 );
 
-   cv::imshow( "Output:", outputImg );
+
+   yolo->loadModel( "../models/v8_objDetection.onnx", false );
+
+   detections = yolo->getFeatureLocationsInImage( inputImg, outputImg );
+
+   for( const auto& detection : detections ) {
+      std::cout << detection << "\n";
+   }
+
+   cv::imshow( "Output (CPU Backend):", outputImg );
    cv::waitKey( 0 );
 
 }
